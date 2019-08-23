d50bbdb0197ce353496fc1289fd552e09946e92b
Jodi Warren <jodi@jodiwarren.com>
2019-08-23 12:55:53 +0100

    Add working post author and handle HTML properly

--- wp-content/plugins/worksim-notify-on-publish/worksim-notify-on-publish.php
+++ wp-content/plugins/worksim-notify-on-publish/worksim-notify-on-publish.php
@@ -3,11 +3,6 @@
  * Plugin Name: Worksim - Email Notify On Publish
  */
 
-
-/**
- * WP_Post Object ( [ID] => 26 [post_author] => 1 [post_date] => 2019-08-23 11:28:10 [post_date_gmt] => 2019-08-23 11:28:10 [post_content] => [post_title] => Hello again [post_excerpt] => [post_status] => publish [comment_status] => closed [ping_status] => closed [post_password] => [post_name] => hello-again [to_ping] => [pinged] => [post_modified] => 2019-08-23 11:28:10 [post_modified_gmt] => 2019-08-23 11:28:10 [post_content_filtered] => [post_parent] => 0 [guid] => http://localhost/?page_id=26 [menu_order] => 0 [post_type] => page [post_mime_type] => [comment_count] => 0 [filter] => raw )
- */
-
 /**
  * Class WorksimNotifyOnPublish
  */
@@ -29,27 +24,42 @@ class WorksimNotifyOnPublish {
 	 */
 	public function onPublish( $post_id, $post ){
 
+		// TODO: Filter these to only the newly published posts, instead of including updated
+
 		// Just in case...
 		if (!$post || !$post_id) {
 			return;
 		}
 
-		$authorName = get_the_author($post->post_author);
+		$admin_email = get_option('admin_email', 'help@dxw.com'); // TODO: Add a proper fallback strategy
 
-		$title = sprintf(
+		$authorName = get_the_author_meta('display_name', $post->post_author);
+
+		$title = wp_sprintf(
 			'Review new %s from %s',
 			$post->post_type,
 			$authorName
 		);
 
-		$post = sprintf(
-			'%s has published a new %s: %s <a href="%s">View it here</a>',
-			$authorName,
-			$post->post_type,
-			$post->title,
-			$post->guid
+		$link = wp_sprintf(
+			'<a href="%s">View it here</a>',
+			esc_url($post->guid)
 		);
-		wp_mail('test@test.com', esc_textarea($title), esc_html($post), array('Content-Type: text/html; charset=UTF-8'));
+
+		$body = wp_sprintf(
+			'%s has published a new %s: %s %s',
+			esc_html($authorName),
+			esc_html($post->post_type),
+			esc_html($post->title),
+			$link
+		);
+
+		wp_mail(
+			sanitize_email($admin_email), // admin email
+			esc_textarea($title), // email title
+			$body, // email body
+			['Content-Type: text/html; charset=UTF-8'] // headers
+		);
 	}
 
 }
